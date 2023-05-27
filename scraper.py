5bddaf8f6052cc5a1724d13e96282b738894f7c3
gukstan <gukstan@gmail.com>
2023-05-27 13:29:13 -0300

    add

--- scraper.py
+++ scraper.py
@@ -5,4 +5,4 @@ premier_2022 = pd.read_csv('https://www.football-data.co.uk/mmz4281/2223/E0.csv'
 
 premier_2022.rename(columns={'FTHG': 'HomeGoals', 'FTAG': 'AwayGoals'}, inplace=True)
 
-premier_2022
+premier_2023
